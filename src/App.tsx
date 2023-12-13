985acf9685c29f925c5d5bbc2182ef5b8e54c558
Mohit <mkjakhar909@gmail.com>
2023-12-13 13:46:33 +0530

    extra code removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoList from "./components/TodoList";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { collection, getDocs } from "firebase/firestore";
@@ -13,60 +13,30 @@ interface TodoData {
 
 function App() {
   const [todoData, setTodoData] = useState<TodoData[]>([]);
-
-  // const getData = () => {
-  //   return getDocs(collection(db, "todo"))
-  //     .then((querySnapshot) => {
-  //       const data = querySnapshot.docs.map(
-  //         (doc) =>
-  //           ({
-  //             id: doc.id,
-  //             ...doc.data(),
-  //           } as TodoData)
-  //       );
-  //       setTodoData(data);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching data:", error);
-  //     });
-  // };
-  const getData = async () => {
-    try {
-      const querySnapshot = await getDocs(collection(db, "todo"));
-
-      const data: TodoData[] = querySnapshot.docs.map(
-        (doc) =>
-          ({
-            id: doc.id,
-            ...doc.data(),
-          } as TodoData)
-      );
-
-      setTodoData(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  getData();
   const completeTask = () => {
     console.log("delete task");
   };
   const deleteTask = () => {
     console.log("delete task");
   };
-  // const todoDatas: TodoData[] = [];
 
-  // const getDatas = async () => {
-  //   const querySnapshot = await getDocs(collection(db, "my-todo"));
-  //   querySnapshot.forEach((doc) => {
-  //     todoData.push({
-  //       id: doc.id,
-  //       ...doc.data(),
-  //     } as TodoData);
-  //   });
-  // };
-  // getDatas();
+  useEffect(() => {
+    const getData = async () => {
+      const querySnapshot = await getDocs(collection(db, "todo"));
+      const array: TodoData[] = [];
+      querySnapshot.forEach((doc) => {
+        array.push({
+          id: doc.id,
+          ...doc.data(),
+        } as TodoData);
+      });
+      setTodoData(array);
+    };
+    getData();
+  }, []);
+
+  console.log(todoData);
+
   return (
     <>
       <TodoList />
